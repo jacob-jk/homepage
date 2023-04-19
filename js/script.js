299c3f28fbe2563618dafc089c1348c997b14793
Kuba Karnath <jacob_jk@wp.pl>
2023-04-19 13:07:25 +0200

    html and js edits

--- js/script.js
+++ js/script.js
@@ -1,34 +1,48 @@
-console.log("Hello world! Ten kod jest już w repozytorium :)");
+{
+    const welcome = () => {
+        console.log("Hello world! Ten kod jest już w repozytorium :)");
+    };
 
-let backgroundButton = document.querySelector(".body__backgroundButton");
-let body = document.querySelector(".body");
-let motiveName = document.querySelector(".body__motiveName");
+    const onChangeBackgroundClick = () => {
+        const body = document.querySelector(".body");
+        const motiveName = document.querySelector(".body__motiveName");
 
-backgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body__motive");
+        body.classList.toggle("body__motive");
+        body.classList.contains("body__motive") ? motiveName.innerText = "jasny" : motiveName.innerText = "ciemny";
 
-    //     if (body.classList.contains("body__motive")) {
-    //         motiveName.innerText = "jasny";
-    //     } else {
-    //         motiveName.innerText = "ciemny";
-    //     }
-    // });
+        //     if (body.classList.contains("body__motive")) {
+        //         motiveName.innerText = "jasny";
+        //     } else {
+        //         motiveName.innerText = "ciemny";
+        //     }
+        // });
+    };
 
-    body.classList.contains("body__motive") ? motiveName.innerText = "jasny" : motiveName.innerText = "ciemny";
-})
+    const init = () => {
+        const backgroundButton = document.querySelector(".body__backgroundButton");
+        backgroundButton.addEventListener("click", onChangeBackgroundClick);
 
-let buttonElement = document.querySelector(".section__button");
-let photoElement = document.querySelector(".section__photo")
+        welcome();
+       
+    };
 
-buttonElement.addEventListener("click", () => {
-    if (photoElement.classList.contains("hidden")) {
-        buttonElement.innerText = "Usuń zdjęcie";
-        photoElement.classList.remove("hidden");
-    } else {
-        photoElement.classList.add("hidden");
-        buttonElement.innerText = "Dodaj zdjęcie";
-    }
-});
+    init();
 
 
 
+    const togglePhoto = () => {
+        const buttonElement = document.querySelector(".section__button");
+        const photoElement = document.querySelector(".section__photo");
+        
+    buttonElement.addEventListener("click", () => {
+        if (photoElement.classList.contains("hidden")) {
+            buttonElement.innerText = "Usuń zdjęcie";
+            photoElement.classList.remove("hidden");
+        } else {
+            photoElement.classList.add("hidden");
+            buttonElement.innerText = "Dodaj zdjęcie";
+        }
+    });
+};
+togglePhoto();
+}
